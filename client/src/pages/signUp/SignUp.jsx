181e65334ec47bc6bde8edefe7f7a434c74e65b5
Syeda-Aisha-Ahmad <18aishaahmad@gmail.com>
2023-11-30 22:22:09 +0600

    complete sign up page functionality

--- client/src/pages/signUp/SignUp.jsx
+++ client/src/pages/signUp/SignUp.jsx
@@ -1,40 +1,90 @@
 import { IoLogoGoogle } from "react-icons/io";
 import './SignUp.css';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useState } from "react";
+
 
 export default function SignUp() {
+    const [formData, setFormData] = useState({});
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(false);
+
+    const navigate = useNavigate();
+
+    const handleChange = (e) => {
+        setFormData(
+            {
+                ...formData,
+                [e.target.id]: e.target.value,
+            }
+        );
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            setLoading(true);
+            const res = await fetch('api/auth/signup',
+                {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(formData),
+                }
+            );
+            const data = await res.json();
+            if (data.success === false) {
+                setLoading(false);
+                setError(data.message);
+                return;
+            }
+            setLoading(false);
+            setError(null);
+            navigate('/sign-in');
+        }
+        catch (error) {
+            setLoading(false);
+            setError(error.message);
+        }
+
+
+    }
     return (
-        <div className="max-w-[1400px] mx-auto loggout-style h-screen py-40 md:py-52 lg:py-10 " >
+        <div className="max-w-[1400px] mx-auto loggout-style h-fit py-40 md:py-52 lg:py-10 " >
 
             <h2 className='text-5xl font-semibold text-center text-white'>Sign Up</h2>
             <div className="lg:grid place-items-center w-11/12 md:w-9/12 lg:w-full mx-auto">
 
 
                 <div className="card shrink-0 lg:w-4/12">
-                    <form className="card-body">
+                    <form onSubmit={handleSubmit} className="card-body">
 
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text text-indigo-200">User name</span>
                             </label>
-                            <input type="text" placeholder="username" id="username" className="input input-bordered" required />
+                            <input type="text" placeholder="username" id="username" onChange={handleChange} className="input input-bordered" required />
                         </div>
 
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text text-indigo-200">Email</span>
                             </label>
-                            <input type="email" placeholder="email" id="email" className="input input-bordered" required />
+                            <input type="email" placeholder="email" id="email" onChange={handleChange} className="input input-bordered" required />
                         </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text text-indigo-200">Password</span>
                             </label>
-                            <input type="password" placeholder="password" id="password" className="input input-bordered" required />
+                            <input type="password" placeholder="password" id="password" onChange={handleChange} className="input input-bordered" required />
 
                         </div>
                         <div className="form-control mt-6">
-                            <button className="btn bg-indigo-500 hover:bg-indigo-600 border-none text-white">Sign up</button>
+                            <button disabled={loading} className="btn bg-indigo-500 hover:bg-indigo-600 border-none text-white">
+                                {loading ? 'Loading...' : 'Sign Up'}
+                            </button>
                         </div>
                         <div className="divider text-indigo-200  after:bg-indigo-200 before:bg-indigo-200">OR</div>
                         <div className="form-control">
@@ -46,6 +96,7 @@ export default function SignUp() {
                                 <span className="text-indigo-200 hover:text-indigo-300"> Sign In</span>
                             </Link>
                         </p>
+                        {error && <p className="bg-red-500 bg-opacity-30 font-bold text-red-200 mt-5">{error}</p>}
                     </form>
                 </div>
 
